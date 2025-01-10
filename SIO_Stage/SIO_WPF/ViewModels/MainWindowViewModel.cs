2154105714a0c67eddfa55fd0ed2a7a2f325568f
Thierno Barry <tibarry10@outlook.com>
2025-01-10 08:44:58 +0100

    8h45

---
+++ SIO_Stage/SIO_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SIO_WPF.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private int nombreSecret;
+        private Random random;
+        private int tentativesRestantes;
+        public string Nombre { get; set; } = "Entrer un nombre";
+        public string Proposition { get; set; }
+        public ICommand PropCommand { get; set; }
+        public ICommand ResetCommand { get; set; }
+        private string reponse;
+        public string Reponse
+        {
+            get => reponse;
+            set => SetProperty(ref reponse, value);
+        }
+
+        private string couleur;
+        public string Couleur
+        {
+            get => couleur;
+            set => SetProperty(ref couleur, value);
+        }
+
+        private string imageSource;
+        public string ImageSource
+        {
+            get => imageSource;
+            set => SetProperty(ref imageSource, value);
+        }
+
+
+        public int TentativesRestantes
+        {
+            get => tentativesRestantes;
+            set => SetProperty(ref tentativesRestantes, value);
+        }
+        public MainWindowViewModel()
+        {
+            PropCommand = new RelayCommand(OnProposition);
+            ResetCommand = new RelayCommand(OnReset);
+            random = new Random();
+            CommenceLeJeu();
+        }
+
+        private void CommenceLeJeu()
+        {
+            nombreSecret = random.Next(1, 101);
+            TentativesRestantes = 7;
+            Proposition = string.Empty;
+            Reponse = string.Empty;
+            Couleur = "Black";
+        }
+
+
+        private void OnProposition(object obj)
+        {
+            if (TentativesRestantes <= 0)
+            {
+                Reponse = "Vous avez perdu ! Cliquez sur Rejouer pour recommencer !";
+                return;
+            }
+
+            if (int.TryParse(Proposition, out int nombreUtilisateur))
+            {
+                if (nombreUtilisateur < nombreSecret)
+                {
+                    Reponse = "Trop pétit";
+                    Couleur = "Blue";
+                    ImageSource = "Images/trop_petit.jpg";
+                }
+                else if (nombreUtilisateur > nombreSecret)
+                {
+                    Reponse = "Trop Grand";
+                    Couleur = "Orange";
+                    ImageSource = "Images/trop_grand.png";
+                }
+                else
+                {
+                    Reponse = "Bravo, c'est le nombre exact !\nVoulez-vous rejouer ?";
+                    Couleur = "Green";
+                    ImageSource = "Images/gagne.jpg";
+                    return;
+                }
+
+                TentativesRestantes--;
+                if (TentativesRestantes <= 0)
+                {
+                    Reponse = $"Vous avez perdu ! le nombre était {nombreSecret}. \nVoulez-vous rejouer ?";
+                    Couleur = "Red";
+                }
+            }
+            else
+            {
+                Reponse = "Veuiller entrer un nombre valide";
+                Couleur = "Gray";
+                ImageSource = null;
+            }
+        }
+
+        //Rénitialiser la partie
+        private void OnReset(object obj)
+        {
+            CommenceLeJeu();
+        }
+    }
+}
