0552de456b0f397592c5cb08fbaaf793aa6286a8
Ali MAKRI <github@makri.fr>
2025-01-14 12:19:42 +0100

    Version fnale

---
+++ SIO_Stage/WPF_Stage/Common/Texte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WPF_Stage.Common
+{
+    public static class Texte
+    {
+        public static bool IsNumeric(this string text)
+        {
+            return Regex.IsMatch(text, "^[0-9]*$"); // Accepte les chaines vides ou contenant uniquement que des chiffres
+        }
+    }
+}
--- SIO_Stage/WPF_Stage/ViewModels/MainWindowViewModel.cs
+++ SIO_Stage/WPF_Stage/ViewModels/MainWindowViewModel.cs
@@ -1,65 +1,138 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Markup;
+using WPF_Stage.Common;
+using WPF_Stage.Properties;
 
 namespace WPF_Stage.ViewModels
 {
     class MainWindowViewModel : ViewModelBase
     {
-        private bool propositionSelectionnee;
-        private int nombreSecret;
-        private Random random;
-        private int tentativesRestantes;
-        private MainWindow mainWindow1;
+        #region Propriétés
+        private bool PropositionSelectionnee;
+        private int NombreSecret;
+        private Random Alea;
+        private MainWindow MainWindow1;
+        private bool FinPartie = false;
+        #endregion
 
-        public ICommand PropCommand { get; set; }
-        public ICommand ResetCommand { get; set; }
-
-        //Indique si la TextBox doit avoir le focus.
-        private bool hasFocus;
-        public bool HasFocus
+        #region Command Binding
+        public ICommand OkCommand { get; set; }
+        private void OkExecute(object obj)
         {
-            get => hasFocus;
-            set => SetProperty(ref hasFocus, value);
+            FinPartie = false;
+
+            if (int.TryParse(Proposition, out int nombreUtilisateur))
+            {
+                TentativesRestantes--;
+                if (TentativesRestantes <= 0)
+                {
+                    Reponse = $"Vous avez perdu ! le nombre était {NombreSecret}. \nVoulez-vous rejouer ?";
+                    Couleur = "Red";
+                    FinPartie = true;
+                }
+                else if (nombreUtilisateur < NombreSecret)
+                {
+                    Reponse = "Trop petit";
+                    Couleur = "Blue";
+                    ImageSource = "../Images/trop_petit.png";
+                }
+                else if (nombreUtilisateur > NombreSecret)
+                {
+                    Reponse = "Trop Grand";
+                    Couleur = "Orange";
+                    ImageSource = "../Images/trop_grand.png";
+                }
+                else
+                {
+                    Reponse = "Bravo, c'est le nombre exact !\nVoulez-vous rejouer ?";
+                    Couleur = "Green";
+                    ImageSource = "../Images/gagne.jpg";
+                    FinPartie = true;
+                }
+                if (FinPartie)
+                {
+                    DefaultOk = false;
+                    DefaultRejouer = true;
+                }
+                else
+                {
+                    DefaultOk = true;
+                    DefaultRejouer = false;
+                    // Donne le focus et sélectionne tout dans la TextBox via la vue
+                    MainWindow1.myTextBox.Focus();
+                    MainWindow1.myTextBox.SelectAll();
+                }
+            }
+            else
+            {
+                Reponse = "Veuiller entrer un nombre valide";
+                Couleur = "Gray";
+                ImageSource = null;
+            }
+        }
+        private bool OkCanExecute(object obj)
+        {
+            return !string.IsNullOrEmpty(proposition) &&
+                !Reponse.Contains("Bravo") &&
+                !Reponse.Contains("perdu");
         }
 
-        
+        public ICommand RejouerCommand { get; set; }
+        public void RejouerExecute(object obj)
+        {
+            Init();
+        }
+        private bool OnRejouerCommandCanExecute(object obj)
+        {
+            // si le joueur a gagné ou perdu on autorise le reset
+            return Reponse.Contains("Bravo") || Reponse.Contains("perdu");
+        }
 
-        private string reponse;
+        #endregion
+
+        #region Binding
         public string Reponse
         {
             get => reponse;
-            set 
-            { 
-                if (SetProperty(ref reponse, value))
-                {
-                    OnPropertyChanged(nameof(CanReset)); // préviens la vue que CanReset a changé
-                    CommandManager.InvalidateRequerySuggested(); // Met à jour l'état de la commande PropCommand
-                } 
-            }
+            set => SetProperty(ref reponse, value);
         }
-        private string proposition;
+        private string reponse;
+
+        public string Libelle
+        {
+            get => libelle;
+            set => SetProperty(ref libelle, value);
+        }
+        private string libelle = string.Empty;
+
+        public bool DefaultOk
+        {
+            get => defaultOk;
+            set => SetProperty(ref defaultOk, value);
+        }
+        private bool defaultOk = true;
+
+        public bool DefaultRejouer
+        {
+            get => defaultRejouer;
+            set => SetProperty(ref defaultRejouer, value);
+        }
+        private bool defaultRejouer = false;
+
         public string Proposition
         {
             get => proposition;
             set
             {
                 //vérifie si la saisie est valide
-                if (string.IsNullOrEmpty(value) || IsTextNumeric(value))
+                if (string.IsNullOrEmpty(value) || value.IsNumeric())
                 {
                     SetProperty(ref proposition, value);
                 }
             }
         }
-
-        //vérifie si une chaine contient uniquement des chiffres
-       
+        private string proposition;
 
         private string couleur;
         public string Couleur
@@ -76,30 +149,34 @@ namespace WPF_Stage.ViewModels
         }
 
 
+        private int tentativesRestantes;
         public int TentativesRestantes
         {
             get => tentativesRestantes;
             set => SetProperty(ref tentativesRestantes, value);
         }
-        public MainWindowViewModel(MainWindow MainWindow1)
+
+        #endregion
+
+        #region Constructeur
+
+        public MainWindowViewModel(MainWindow mainWindow1)
         {
-            mainWindow1 = MainWindow1;
-            PropCommand = new RelayCommand(OnProposition, OnPropositionCanExecute);
-            ResetCommand = new RelayCommand(OnReset, CanAlwaysExecute);
-            random = new Random();
-            CommenceLeJeu();
+            // Lien avec la View
+            MainWindow1 = mainWindow1;
+            Alea = new Random();
+            Libelle = $"Devinez le nombre entre {Settings.Default.Min} et {Settings.Default.Max}";
+
+            // Command
+            OkCommand = new RelayCommand(OkExecute, OkCanExecute);
+            RejouerCommand = new RelayCommand(RejouerExecute, OnRejouerCommandCanExecute);
+
+            Init();
         }
-
-        private bool CanAlwaysExecute(object obj)
+        private void Init()
         {
-            return true; // Le bouton Rejouer reste toujours activé
-        }
-
-
-        private void CommenceLeJeu()
-        {
-            nombreSecret = random.Next(1, 101);
-            TentativesRestantes = 7;
+            NombreSecret = Alea.Next(Settings.Default.Min, Settings.Default.Max + 1);
+            TentativesRestantes = Settings.Default.CoupMax;
             Proposition = string.Empty;
             Reponse = string.Empty;
             Couleur = "Black";
@@ -107,85 +184,9 @@ namespace WPF_Stage.ViewModels
 
         }
 
-        private bool OnPropositionCanExecute(object obj)
-        {
-            return !string.IsNullOrEmpty(proposition) &&
-                !Reponse.Contains("Bravo") &&
-                !Reponse.Contains("perdu");
-        }
+        #endregion
 
-
-        private void OnProposition(object obj)
-        {
-            if (TentativesRestantes <= 0)
-            {
-                Reponse = "Vous avez perdu ! Cliquez sur Rejouer pour recommencer !";
-                return;
-            }
-
-            if (int.TryParse(Proposition, out int nombreUtilisateur))
-            {
-                if (nombreUtilisateur < nombreSecret)
-                {
-                    Reponse = "Trop petit";
-                    Couleur = "Blue";
-                    ImageSource = "../Images/trop_petit.png";
-                }
-                else if (nombreUtilisateur > nombreSecret)
-                {
-                    Reponse = "Trop Grand";
-                    Couleur = "Orange";
-                    ImageSource = "../Images/trop_grand.png";
-                }
-                else
-                {
-                    Reponse = "Bravo, c'est le nombre exact !\nVoulez-vous rejouer ?";
-                    Couleur = "Green";
-                    ImageSource = "../Images/gagne.jpg";
-                    return;
-                }
-
-                TentativesRestantes--;
-                if (TentativesRestantes <= 0)
-                {
-                    Reponse = $"Vous avez perdu ! le nombre était {nombreSecret}. \nVoulez-vous rejouer ?";
-                    Couleur = "Red";
-                }
-            }
-            else
-            {
-                Reponse = "Veuiller entrer un nombre valide";
-                Couleur = "Gray";
-                ImageSource = null;
-            }
-            // Donne le focus et sélectionne tout dans la TextBox via la vue
-            mainWindow1.myTextBox.Focus();
-            mainWindow1.myTextBox.SelectAll();
-
-
-        }
-
-        private bool IsTextNumeric(string text)
-        {
-            return Regex.IsMatch(text, "^[0-9]*$"); // Accepte les chaines vides ou contenant uniquement que des chiffres
-        }
-
-        public bool CanReset
-        {
-            get
-            {
-                // si le joueur a gagné ou perdu on autorise le reset
-                return Reponse.Contains("Bravo") || Reponse.Contains("perdu");
-            }
-        }
-
-        //Rénitialiser la partie
-        public void OnReset(object obj)
-        {
-            CommenceLeJeu();
-            // Donne le focus et sélectionne tout dans la TextBox via la vue
-            mainWindow1.myTextBox.Focus();
-            mainWindow1.myTextBox.SelectAll();
-        }
+        #region Méthodes
+        #endregion
     }
 }
