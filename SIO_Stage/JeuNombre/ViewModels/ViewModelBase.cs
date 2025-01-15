e1ef989c8ab70aefe26d530e9b2a7485420853c3
Thierno Barry <tibarry10@outlook.com>
2025-01-15 01:42:22 +0100

    #refaire

---
+++ SIO_Stage/JeuNombre/ViewModels/ViewModelBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuNombre.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+
+            this.OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+
+        {
+
+            var handler = this.PropertyChanged;
+
+            if (handler != null)
+
+            {
+
+                handler(this, e);
+
+            }
+
+        }
+
+        #endregion
+    }
+}
